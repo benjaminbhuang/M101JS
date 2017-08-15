54ce1440fbd0a64b7c995282962a0ac17903ae48
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-14 21:31:42 -0400

    add get variables

--- week1/index.js
+++ week1/index.js
@@ -1,20 +1,57 @@
 var express = require('express'),
     app = express(),
-    engines = require('consolidate');
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.get('/', function (req, res) {
-    res.render('hello', {'name': 'Templates'});
+
+function errorHandler(err, req, res,next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500);
+    res.render('error_template', {error: err});
+}
+
+app.use(errorHandler);
+
+app.get('/:name', function (req, res, next) {
+
+    var name = req.params.name;
+    var getvar1 = req.query.getvar1;
+    var getvar2 = req.query.getvar2;
+
+    res.render('hello', {name: name, getvar1: getvar1, getvar2: getvar2});
 });
 
-app.use(function (req, res) {
-    res.sendStatus(404);
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+
+    app.get('/', function(req, res){
+
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.render('movies', { 'movies': docs } );
+        });
+
+    });
+
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(8000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
 });
 
-var server = app.listen(4000, function () {
-    var port = server.address().port;
-    console.log('Express server listening on port %s', port);
-});
+
+
+
