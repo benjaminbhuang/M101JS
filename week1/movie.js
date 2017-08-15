eef8e56c2b7021083b35806fb9e154dce18866f8
Benjamin Huang <bhuang@hms.com>
2017-08-15 14:12:21 -0400

    clean up hw1-4

--- week1/movie.js
+++ week1/movie.js
@@ -8,62 +8,61 @@ var express = require('express'),
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 function errorHandler(err, req, res, next) {
     console.error(err.message);
     console.error(err.stack);
-    res.status(500);
-    res.render('error_template', {'error': err});
+    res.status(500).render('error_template', {'error': err});
 }
 
-app.use(errorHandler);
 
+MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
 
-app.get('/', function (req, res, next) {
-    res.render('enter_movies');
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+
+    app.get('/', function (req, res, next) {
+        res.render('enter_movies');
+    });
+
+    app.post('/enter_movies', function (req, res, next) {
+        var title = req.body.title;
+        var year = req.body.year;
+        var imdb = req.body.imdb;
+
+        if (title === '' || year === '' || imdb === '') {
+            next('all fields required!');
+
+        } else {
+            var movie = {'title': title, 'year': year, 'imdb': imdb};
+
+            db.collection('movies').insertOne(movie
+                , function (err, result) {
+                    assert.equal(null, err);
+                    db.collection('movies').find({}).toArray(function (err, docs) {
+                        assert.equal(null, err);
+                        res.render('movies', {'movies': docs, 'resultid': result.insertId});
+                    });
+                });
+        }
+
+    });
+
+    app.use(errorHandler);
+
+    app.use(function (req, res) {
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(8000, function () {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
+
 });
 
-app.post('/enter_movies', function (req, res, next) {
-    var title = req.body.title;
-    var year = req.body.year;
-    var imdb = req.body.imdb;
-
-    if (typeof title == 'undefined' || title == '' ||
-        typeof year == 'undefined' || year == '' ||
-        typeof  imdb == 'undefined' || imdb == ''
-    ) {
-
-        next(Error('all fields required!'));
-
-    } else {
-        var movie = {'title': title, 'year': year, 'imdb': imdb};
-
-        MongoClient.connect('mongodb://localhost:27017/video', function (err, db) {
-
-            assert.equal(null, err);
-            console.log("Successfully connected to MongoDB.");
-
-            db.collection('movies').insertOne(movie);
-
-            db.collection('movies').find({}).toArray(function (err, docs) {
-                res.render('movies', {'movies': docs});
-            });
-
-        });
-    }
-});
-
-
-app.use(function (req, res) {
-    res.sendStatus(404);
-});
-
-var server = app.listen(8000, function () {
-    var port = server.address().port;
-    console.log('Express server listening on port %s.', port);
-});
 
 
 
