599937df2c1068e04f8002aa16f1dd6d42a7ca30
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-14 20:32:08 -0400

    week one mongodb driver

--- week1/app.js
+++ week1/app.js
@@ -1,11 +1,27 @@
-var http = require('http');
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
-var server = http.createServer(
-    function(req, res){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World!');
+var url = 'mongodb://localhost:27017/video';
+
+MongoClient.connect(url, function(err, db) {
+
+    assert.equal(null, err);
+    console.log("Successfully connected to server");
+
+    // Find some documents in our collection
+    db.collection('movies').find({}).toArray(function(err, docs) {
+
+        // Print the documents returned
+        docs.forEach(function(doc) {
+            console.log(doc.title);
+        });
+
+        // Close the DB
+        db.close();
     });
 
-server.listen(8000);
+    // Declare success
+    console.log("Called find()");
+});
 
-console.log("Server is running at http://localhost:8000");
+
